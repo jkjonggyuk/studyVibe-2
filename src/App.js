657b61309c0a11135bb79ee0209f9e701477527a
JK Kim <jk.jonggyuk@gmail.com>
2021-04-24 15:03:49 -0700

    added react helmet for better google SEO. ready to deploy

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 // import logo from './logo.svg';
 import './App.css';
 import { HashRouter, Switch, Route } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import Home from "./components/pages/Home";
 // import NameDialog from "./components/NameDialog";
 // import GridList from "./components/GridList";
@@ -11,13 +12,15 @@ import Home from "./components/pages/Home";
 function App() {
   return (
     <>
-    
+    <Helmet>
+      <title>StudyVIBE</title>
+      <meta name="description" content="The minimalistic Integrated Study Environment solution for your perfect VIBE for studying" />
+    </Helmet>
+
     <HashRouter>
       {/* <Navbar /> */}
       {/* <NameDialog /> */}
       {/* <Sidebar /> */}
-
-
       <Switch>
           <Route exact path='/' component={Home}/>
       </Switch>
