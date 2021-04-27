da9082d60247060c445e11d35f906ff1cda80005
JK Kim <jk.jonggyuk@gmail.com>
2021-04-26 22:44:45 -0700

    removed helmet

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 // import logo from './logo.svg';
 import './App.css';
 import { HashRouter, Switch, Route } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
 import Home from "./components/pages/Home";
 // import NameDialog from "./components/NameDialog";
 // import GridList from "./components/GridList";
@@ -12,11 +11,7 @@ import Home from "./components/pages/Home";
 function App() {
   return (
     <>
-    <Helmet>
-      <title>StudyVIBE</title>
-      <meta name="description" content="The minimalistic Integrated Study Environment solution for your perfect VIBE for studying" />
-    </Helmet>
-
+    
     <HashRouter>
       {/* <Navbar /> */}
       {/* <NameDialog /> */}
